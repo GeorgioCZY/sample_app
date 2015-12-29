ed08ce48ec46c52216b623c29865c99352d01fbe
George Cao <george.cao@flipp.com>
2015-12-29 13:29:30 -0500

    added more tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,17 +2,6 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test 'invalid signup information' do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  '',
-                               email: 'user@invalid',
-                               password:              'foo',
-                               password_confirmation: 'bar' }
-    end
-    assert_template 'users/new'
-  end
-
   test 'valid signup information' do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -22,5 +11,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: 'password' }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
+
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  '',
+                               email: 'user@invalid',
+                               password:              'foo',
+                               password_confirmation: 'bar' }
+    end
+    assert_template 'users/new'
+    assert_select 'div#<error_explanation>'
+    assert_select 'div.<alert alert-danger>'
+  end
+
 end
